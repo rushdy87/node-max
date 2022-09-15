4d69f2e4d5448836a6d47887cfc3f48392c191f3
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-09-15 23:06:50 +0300

    Working with Dynamic Content: Adding Templating Engines - EJS

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
 app.listen(3000);
--- ecommerce/routes/shop.js
+++ ecommerce/routes/shop.js
@@ -8,8 +8,14 @@ const products = require('./admin').products;
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
